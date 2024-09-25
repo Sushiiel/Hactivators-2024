c74412f6628ce6deef920f35abfdfdb1564f7491
Deena Athithyaa D <deenaathithyaa.d2022@vitstudent.ac.in>
2024-09-25 09:42:03 +0530

    tailwind

--- hactivators/src/App.js
+++ hactivators/src/App.js
@@ -1,21 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <header className="text-center">
+        <img src={logo} className="h-40 animate-spin-slow" alt="logo" />
+        <p className="text-lg mt-4">
+          Edit <code className="bg-gray-200 px-2 py-1 rounded">src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-blue-500 hover:text-blue-700 text-xl mt-4 inline-block"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-         Hactivators
+          Hactivators
         </a>
       </header>
     </div>
